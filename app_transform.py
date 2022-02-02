0a2c264185c68032c20419bf431aa35f7bd2f926
bennbenben <80408902+bennbenben@users.noreply.github.com>
2022-02-02 17:11:42 +0800

    Update app_transform.py

--- app_transform.py
+++ app_transform.py
@@ -7,50 +7,71 @@ from collections import defaultdict
 ### Helper functions ###
 
 ### Initialize variables ###
-ig_defined_res = (1080.0, 1920.0)
-resize_factor = .8
+ig_defined_res = (1080.0, 1920.0) # give in a float value
+screen_factor = .8
+scale_tolerance=2
 img_vars = defaultdict()
 
-# file_path = "C:\\Users\\bennb\\Desktop\\yoda.jpg"
-
-
-# Algo
-# func 1. Read in image dimensions (esp width). Output a string: shrink or scale
-# func 2. Resize according to pre-defined IG dimensions while maintaining aspect ratio (a percentage)
-# func 3. Image padding
+# Flow
+# func 1. Read in image dimensions (esp width). Output a dictionary of variables to pass to func2
+# func 2. Resize according to a percentage of pre-defined IG dimensions, while maintaining aspect ratio. Output a dictionary of variables to pass to func3
+# func 3. Calculate the padding the images need and pass the variables to func4
+# func 4. Execute the resize, and the image padding steps. Output the new image in a saved location
 
 ### Application functions ###
-def shrink_or_scale(file_path: str, defined_res: tuple)->dict:
+def resize_type(file_path: str, defined_res: tuple, screen_factor: float, scale_tolerance: float)->dict:
 	"""
 	Reads in an image and compares its width with a set of pre-defined resolutions
 	Returns a dictionary of 2 key-value pairs: 
 	{img_vars['img_obj']: cv2-read image object,
-	img_vars['img_res']: (width as int, height int),
-	img_vars['defined_res']: (width as int, height int),
-	img_vars['resize_dir']: string}
+	img_vars['img_res']: (width as float, height as float),
+	img_vars['img_aspect_ratio']: float,
+	img_vars['defined_res']: (width as float, height float),
+	img_vars['screen_factor']: float,
+	img_vars['proposed_res']: (width as float, height as float),
+	img_vars['proposed_resize_dir']: 'scale' or 'shrink' as string,
+	img_vars['scale_tolerance']: float,
+	img_vars['proposed_resize_factor']: float,
+	img_vars['resize_validity']: boolean (None if proposed_resize_dir='shrink'. True/False if proposed_resize_dir='scale')}
 	"""
-	# Read image and load dimensions into img_vars dictionary
+	# Read image resolutions and aspect ratio, load these variables into img_vars dictionary
 	img_vars['img_obj'] = cv2.imread(file_path)
 	img_vars['img_res']= tuple(map(float,img_vars['img_obj'].shape[:2]))
+	img_vars['img_aspect_ratio'] = img_vars['img_res'][0] / img_vars['img_res'][1]
 	img_vars['defined_res'] = defined_res
+	img_vars['screen_factor'] = screen_factor
 
-	# Compare image width with pre-defined width
-	if img_vars['img_res'][0] < img_vars['defined_res'][0]: img_vars['resize_dir'] = 'scale'
-	else: img_vars['resize_dir'] = 'shrink'
+	# Propose a resized image resolution (whether its scale or shrink)
+	proposed_width = screen_factor * img_vars['defined_res'][0] # 1080 * 0.8 = 864
+	proposed_height = proposed_width / img_vars['img_aspect_ratio'] # 864 / 0.8 = 1080
+
+	if proposed_height <= defined_res[1]: pass
+	else:
+		proposed_height = screen_factor * img_vars['defined_res'][1]
+		proposed_width = proposed_height * img_vars['img_aspect_ratio']
+
+	img_vars['proposed_res'] = (proposed_width, proposed_height)
+	if img_vars['proposed_res'][0] <= img_vars['img_res'][0] : img_vars['proposed_resize_dir'] = 'shrink'
+	else: img_vars['proposed_resize_dir'] = 'scale'
+
+	# Check if proposed resize is within specified tolerance
+	img_vars['scale_tolerance'] = scale_tolerance
+	img_vars['proposed_resize_factor'] = img_vars['proposed_res'][0] / img_vars['img_res'][0]
+
+	if img_vars['proposed_resize_dir'] == 'scale':
+		if img_vars['proposed_resize_factor'] <= img_vars['scale_tolerance']: img_vars['resize_validity'] = True
+		else: img_vars['resize_validity'] = False
+
+	if img_vars['proposed_resize_dir'] == 'shrink': img_vars['resize_validity'] = None
 
 	return img_vars
 
-def image_resize(img_vars: dict, resize_factor: float)->dict:
+def add_padding_vars(img_vars: dict)->dict:
 	"""
-	Takes in dictionary of image variables. Calculates and performs image resizing according to the element 'resize_dir' and 'resize_factor'
-	If any of the resized dimension exceed that of defined dimension, then resize to the maximum of the limiting dimension, keeping aspect ratio constant
-
+	Takes in image varibles from resize_type function output in the form of a dictionary. Calculates the image padding and appends its details to the dictionary
 	"""
-	img_vars['resize_factor']=resize_factor
-	img_vars['resized_res']=tuple(i*resize_factor for i in img_vars['img_res'])
-
-	return img_vars
-
+	# Still development
+	return
 
 ### Application Main ###
 if __name__=="__main__":
@@ -62,7 +83,6 @@ if __name__=="__main__":
 
 	# Start Application
 	for i in args.string_1:
-		resize_direction=shrink_or_scale(i, ig_defined_res)
-		# print(resize_direction)
-		resize_result=image_resize(resize_direction, resize_factor)
-		print(resize_result)
+		resize_vars = resize_type(i, ig_defined_res, screen_factor, scale_tolerance)
+		img_vars = add_padding_vars(resize_vars)
+		
