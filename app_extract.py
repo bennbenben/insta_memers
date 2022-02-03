853df6c33cc4e96f616e8deca7b150ee36616ace
bennbenben <80408902+bennbenben@users.noreply.github.com>
2022-02-04 01:53:25 +0800

    Update app_extract.py

--- app_extract.py
+++ app_extract.py
@@ -12,6 +12,7 @@ from imgurpython import ImgurClient
 import configparser
 import argparse
 from collections import defaultdict
+from datetime import datetime
 
 ### Helper functions ###
 def valid_date(s):
@@ -19,9 +20,9 @@ def valid_date(s):
 	Helper function that checks the command line input that execution_date is specified correctly. In this case: %Y-%m-%d
 	"""
 	try:
-		return datetime.strptime(s, "%Y-%m-%d_%H-%M")
+		return datetime.strptime(s, "%Y%m%d_%H%M")
 	except ValueError:
-		err_msg = "not a valid datetime: {0}. Use this format:{1}".format(s,"%Y-%m-%d_%H-%M")
+		err_msg = "not a valid datetime: {0}. Use this format:{1}".format(s,"%Y%m%d_%H%M")
 		raise argparse.ArgumentTypeError(err_msg)
 
 ### Initialize variables ###
@@ -38,8 +39,8 @@ def auth(auth_config_file_path: str)->object:
 	# Obtain login variables from authentication file
 	config=configparser.ConfigParser()
 	config.read(auth_config_file_path)
-	client_id=config('credentials','client_id')
-	client_secret=config('credentials','client_secret')
+	client_id=config.get('credentials','client_id')
+	client_secret=config.get('credentials','client_secret')
 	imgur_username = config.get('credentials','imgur_username')
 	imgur_password = config.get('credentials','imgur_password')
 
@@ -47,42 +48,49 @@ def auth(auth_config_file_path: str)->object:
 	client = ImgurClient(client_id=client_id, client_secret=client_secret)
 	auth_url = client.get_auth_url(response_type='pin')
 
-	# Initialize selenium 
+	# Selenium handling 
 	## Login to imgur authentication website
-	driver = webdriver.Chrome(Service('C:\\Program Files (x86)\\chromedriver.exe'))
+	driver = webdriver.Chrome(service=Service('C:\\Program Files (x86)\\chromedriver.exe'))
 	driver.get(auth_url)
-	username = driver.find_element((By.XPATH,'//*[@id="username"]'))
-	password = driver.find_element((By.XPATH,'//*[@id="password"]'))
+	username = driver.find_element(By.XPATH,'//*[@id="username"]')
+	password = driver.find_element(By.XPATH,'//*[@id="password"]')
 	username.clear()
 	username.send_keys(imgur_username)
 	password.send_keys(imgur_password)	
-	driver.find_element_by_name("allow").click()
+	driver.find_element(By.NAME, "allow").click()
 
 	## Obtain pin from imgur website
 	wait = WebDriverWait(driver, 20)
 	wait.until(EC.presence_of_element_located((By.ID, 'pin')))
-	pin=driver.find_element((By.XPATH, '//*[@id="pin"]')).get_attribute('value')
+	pin=driver.find_element(By.XPATH, '//*[@id="pin"]').get_attribute('value')
 	driver.close() # close selenium after obtaining the pin value
 
 	# Set the imgur client instance with authorized pin. Obtain access and refresh tokens
-	credentials=client.authorize(pin,grant_type='pin')
+	credentials=client.authorize(pin,grant_type='pin') # credentials is a dictionary
 	client.set_user_auth(credentials['access_token'], credentials['refresh_token'])
 	print('Authentication success')
-	
+	# print(credentials, type(credentials))
+	# print(client)
+
 	return client
 
 if __name__ == "__main__":
-	my_parser=argparse.ArgumentParser(prog="app_extract.py", description="Python script that connects to Imgur and downloads, and rearrange memes (within Imgur account). For personal use", 
-		usage='%(prog)s execution_date(format:%Y-%m-%d_%H-%M) "authentication_file_path" "dl_dir"')
-	my_parser.add_argument("execution_date", help="Input execution datetime as %Y-%m-%d_%H-%M", type=valid_date)
+
+	# Parse arguments
+	my_parser = argparse.ArgumentParser(prog='app_extract.py', description='Python script that connects to Imgur and downloads, and rearrange memes (within Imgur account). For personal use', usage='%(prog)s execution_date: object format:%%Y-%%m-%%d_%%H-%%M "authentication_file_path: str" "dl_directory_file_path: str" number_of_memes: int')
+
+	my_parser.add_argument("execution_date", help="Input execution datetime as %Y%m%d_%H%M", type=valid_date)
 	my_parser.add_argument("authentication_file_path", help="Input absolute file path to authentication file", type=str)
 	my_parser.add_argument("dl_dir", help="Input download directory to store memes", type=str)
 	my_parser.add_argument("num_of_memes", help="Number of memes to download", type=int)
-	args=my_parser.parse_args()
+	my_args=my_parser.parse_args()
 
-	auth(args.authentication_file_path)
+	print(my_args) # check arguments
 
-	for i in args.num_of_memes:
+	# Start application
+	c = auth(my_args.authentication_file_path)
+
+	# for i in args.num_of_memes:
 
 # Algo
 # 1. Authenticate with Imgur - obtain response pin
@@ -100,4 +108,3 @@ if __name__ == "__main__":
 # 		max_item = i
 # 		max_views = i.views
 # print(max_item.title)
-
